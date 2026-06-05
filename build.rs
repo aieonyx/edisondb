8c578b58a3baa17c8d81db66012bebe4bb30d82e
Ed <eblepiten@gmail.com>
2026-06-05 06:46:52 +0200

    fix(build): tonic-prost-build, unsafe set_var for Rust 1.94 compat

--- build.rs
+++ build.rs
@@ -2,14 +2,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let protoc_path = protoc_bin_vendored::protoc_bin_path()
         .expect("protoc-bin-vendored: failed to locate bundled protoc binary");
 
-    std::env::set_var("PROTOC", protoc_path);
+    unsafe { std::env::set_var("PROTOC", protoc_path); }
 
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
-        .out_dir("src/proto_gen")
-        .compile_protos(&["proto/edisondb.proto"], &["proto/"])
-        .expect("tonic_build: failed to compile proto/edisondb.proto");
+    tonic_prost_build::compile_protos("proto/edisondb.proto")?;
 
     println!("cargo:rerun-if-changed=proto/edisondb.proto");
     println!("cargo:rerun-if-changed=build.rs");
